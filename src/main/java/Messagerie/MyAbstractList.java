4ac16db3be683ce6b947b1280bb48ab7d5e9f04f
21507124 <21507124@209-04.ens.fsi>
2016-05-04 14:07:22 +0200

    Ajouts des classes Personne et MyAbstractList

---
+++ src/main/java/Messagerie/MyAbstractList.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Messagerie;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractListModel;
+
+/**
+ *
+ * @author 21507124
+ */
+public class MyAbstractList extends AbstractListModel<Personne> {
+
+    private List<Personne> myList = new ArrayList<>();
+
+    @Override
+    public int getSize() {        
+        return myList.size();
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Personne getElementAt(int index) {       
+        //   throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return myList.get(index);
+    }
+
+    void add(Personne p) {
+        myList.add(p);
+        this.fireContentsChanged(this, 0, myList.size());
+    }
+}
---
+++ src/main/java/Messagerie/Personne.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Messagerie;
+
+/**
+ *
+ * @author 21507124
+ */
+class Personne {
+    private String nom;
+    private String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    
+    public String getNomPrenom(){        
+        return (nom+" "+prenom);
+    }    
+    // Getters et setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    @Override
+    public String toString() {
+        return getNomPrenom(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
