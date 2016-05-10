fd31e43a14abd60b36e6e896bfa4c4318a28d2ff
Tist0 <jerome6500@gmail.com>
2016-05-10 17:22:05 +0200

    Création du main

---
+++ src/main/java/fr/stri/ConnexionBDD.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.stri;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jerome
+ */
+public class ConnexionBDD {
+  public static void main(String[] args) {      
+    try {
+      Class.forName("org.postgresql.Driver");
+      System.out.println("Driver O.K.");
+
+      String url = "jdbc:postgresql://localhost:5432/JAVA";
+      String user = "postgres";
+      String passwd = "stri";
+
+      Connection conn = DriverManager.getConnection(url, user, passwd);
+      System.out.println("Connexion effective !");         
+         
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }      
+  }    
+}
