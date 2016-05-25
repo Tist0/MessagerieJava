d58bdf9143f8c358da34137085c2ea9e047f02c3
orobassss <fb.blanchet@laposte.net>
2016-05-25 16:56:26 +0200

    modif applif connexionBDD pour de bon ??!!!

--- src/main/java/fr/stri/Acceuil.java
+++ src/main/java/fr/stri/Acceuil.java
@@ -72,7 +72,7 @@ public class Acceuil extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        Identification.main();
+       // Identification.main();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
